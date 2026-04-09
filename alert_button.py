662c7c7a7031b4791c07ab2135bad32ddaa57317
albertweegi81@gmail.com <albertweegi81@gmail.com>
2026-04-09 12:02:02 -0600

    completed assignment

--- alert_button.py
+++ alert_button.py
@@ -1,4 +1,5 @@
 import time
+import requests
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -8,6 +9,7 @@ while True:
     if GPIO.input(7) == GPIO.HIGH and not button_pressed:
         print("Someone pressed the alert button!")
         button_pressed = True
+        requests.get ("https://api.telegram.org/bot8790379477:AAHfnxpjzHuPf60SumRtaQK1e3-tDq1w7W0/sendmessage"),{"id": "8790379477","text": "Kingsley"}
     elif GPIO.input(7) == GPIO.LOW:
         button_pressed = False
     time.sleep(0.1)
